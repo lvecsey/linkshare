a9bef5961da895278d2d4877748a86d641e582ff
Lester Vecsey <lvecsey@gmail.com>
2017-06-19 07:22:25 -0400

    change away from the generic Example internally vote.c

--- vote.c
+++ vote.c
@@ -93,17 +93,25 @@ int vote_decrement(char *json_str) {
 
 }
 
+typedef struct {
+
+  char *title;
+  char *url;
+  
+  critbit0_tree *ls_entries;
+  
+} rework_t;
+
 int rework_voting(const char *str, void *extra) {
 
+  rework_t *rework = (rework_t*) extra;
+  
   char *hexval;
 
   char *ptr;
 
   int retval;
 
-  char *title = "Example";
-  char *url = "https://www.example.com/";
-  
   if (str!=NULL) {
 
     ptr = strstr(str, "\"sort\":\"");
@@ -115,14 +123,14 @@ int rework_voting(const char *str, void *extra) {
       ptr += 8;
       hexval = ptr;
 
-      sprintf(delbuf, "{\"sort\":\"%s\",\"title\":\"%s\",\"url\":\"%s\"}", hexval, title, url);
+      sprintf(delbuf, "{\"sort\":\"%s\",\"title\":\"%s\",\"url\":\"%s\"}", hexval, rework->title, rework->url);
       
       vote_increment_core(hexval);
 
-      sprintf(insbuf, "{\"sort\":\"%s\",\"title\":\"%s\",\"url\":\"%s\"}", hexval, title, url);
+      sprintf(insbuf, "{\"sort\":\"%s\",\"title\":\"%s\",\"url\":\"%s\"}", hexval, rework->title, rework->url);
       
-      critbit0_insert(extra, insbuf);
-      critbit0_delete(extra, delbuf);      
+      critbit0_insert(rework->ls_entries, insbuf);
+      critbit0_delete(rework->ls_entries, delbuf);      
       
     }
     
@@ -136,11 +144,13 @@ int upvote(critbit0_tree *ls_entries, char *title, char *url) {
 
   char strbuf[512];
 
+  rework_t rework = { .title = title, .url = url, .ls_entries = ls_entries };
+  
   int retval;
   
   sprintf(strbuf, "{\"title\":\"%s\",\"url\":\"%s\",\"sort\":\"", title, url);
-
-  retval = critbit0_allprefixed(ls_entries, strbuf, rework_voting, ls_entries);
+  
+  retval = critbit0_allprefixed(ls_entries, strbuf, rework_voting, &rework);
   
   return 0;
   
